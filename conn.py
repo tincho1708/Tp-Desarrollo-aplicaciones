c414fc3b822fbe2357b544fe4d908ed13ae594e5
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2026-06-05 10:19:02 -0300

    conexion empezada

--- conn.py
+++ conn.py
@@ -1,7 +1,23 @@
 import sqlite3 as sq
+from D&D.py import Heroe
 
 class Database:
-    def __init__(self, db_name = "dnd.db")
+    def __init__(self, db_name = 'campania_dnd.db'):
         self.db_name = db_name
-    def crear_base_dnd(Self):
-        conexion = sq.connect(Self.db_name)
+    
+    def conectar(self):
+        conexion = sq.connect(self.db_name)
+        conexion.execute("PRAGMA foreign_keys = ON;")
+        return conexion
+
+class HeroeRepo:
+    def __init__(self):
+        self.db = Database()
+    def create(self, heroe: Heroe):
+        conn = self.db.conectar()
+        cursor = conn.cursor()
+        query = "INSERT INTO heroes (nombre, clase, raza, nivel) VALUES (?, ?. ?)"
+        cursor.execute(query, (heroe.nombre, heroe.clase, heroe.raza, heroe.nivel))
+        conn.commit()
+        heroe.id = cursor.lastrowid
+        conn.close
