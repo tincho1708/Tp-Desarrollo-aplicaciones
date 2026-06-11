a6fae97baf2d5c65b2223bbb0cd89e2865097196
tincho1708 <safarmartinb@gmail.com>
2026-06-11 15:00:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.subheader("gestion profesional de papas")
 
 nombre = st.text_input("Nombre de la papa:")
 if st.button("Saludar"):
-    st.write(f"Hola {nombre}, bienvenido a su papa"s)
+    st.write(f"Hola {nombre}, bienvenido a su papa")
